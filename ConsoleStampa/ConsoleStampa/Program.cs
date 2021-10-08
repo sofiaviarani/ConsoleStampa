2d054360c9756463d9b79e9a769642be59f66c47
Sofia Viarani <SOFIA.VIARANI@local.ittcesena.it>
2021-10-08 09:09:59 +0200

    v1.1

---
+++ ConsoleStampa/ConsoleStampa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleStampa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
